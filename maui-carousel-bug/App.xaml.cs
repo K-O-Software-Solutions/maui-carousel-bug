ded4108af4dd35c71f25f7aaea8dc6fc2e797650
Oscar <oscar.ploeger@gmail.com>
2024-06-09 10:16:16 +0200

    :tada:

---
+++ maui-carousel-bug/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace maui_carousel_bug;
+
+public partial class App : Application
+{
+  public App()
+  {
+    InitializeComponent();
+
+    MainPage = new AppShell();
+  }
+}
---
+++ maui-carousel-bug/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace maui_carousel_bug;
+
+public partial class AppShell : Shell
+{
+  public AppShell()
+  {
+    InitializeComponent();
+  }
+}
